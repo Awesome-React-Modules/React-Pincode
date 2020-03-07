ff05ea0086fd4072e6daa27ca1e5ed1c723a7f9d
Habeeba Naaz <habeebanaaz1@gmail.com>
2020-03-07 10:19:34 -0500

    Added Css to Input Fields for react-pincode

--- test-server/src/App.js
+++ test-server/src/App.js
@@ -5,7 +5,24 @@ import Pincode from "react-pincode";
 function App() {
   return (
     <div className="App">
-      <Pincode />
+      <Pincode
+        Container={{ border: "8px solid #a2a8a8" }}
+        pincodeInput={{
+          width: "400px",
+          height: "50px",
+          border: "5px solid #a2a8a8"
+        }}
+        cityInput={{
+          width: "400px",
+          height: "50px",
+          border: "5px solid #a2a8a8"
+        }}
+        stateInput={{
+          width: "400px",
+          height: "50px",
+          border: "5px solid #a2a8a8"
+        }}
+      />
     </div>
   );
 }
