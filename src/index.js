a15be696cd92656f8f39427396c574ca4cfee33b
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2020-03-19 12:47:50 +0530

    Implementation of global pincodes (#38)
    * Implementation for global pincodes
    
    * Removed repetitive fields coming from dataset on global pincodes

--- src/index.js
+++ src/index.js
@@ -7,82 +7,144 @@ class Pincode extends Component {
 
     this.state = {
       pincode: "",
-      city: "",
-      state: "",
       error: ""
     };
   }
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-    if (e.target.value.length === 6) {
-      this.setState({
-        error: ""
+
+ removeDuplicates(originalArray, prop) {
+  var newArray = [];
+  var lookupObject = {};
+
+  for (let i in originalArray) {
+    lookupObject[originalArray[i][prop]] = originalArray[i];
+  }
+
+  for (let i in lookupObject) {
+    newArray.push(lookupObject[i]);
+  }
+  return newArray;
+  }
+  
+  outputHTML(Info) {
+    if (Info.length == 0) {
+      document.getElementById("city").innerHTML = `<option value="default">City</option>`;
+      document.getElementById("state").innerHTML = `<option value="default">State</option>`;
+      return;
+    }
+    
+    if (Info.length >= 1) {
+      let returnCities = "";
+      let returnStates = "";
+      for (let i = 0; i < Info.length; i++) {
+        returnCities = returnCities + `<option value="${Info[i].CITY}">${Info[i].CITY}</option>`;
+        returnStates = returnStates + `<option value="${Info[i].STATE}">${Info[i].STATE}</option>`;
+
+      }
+
+      document.getElementById("city").innerHTML = returnCities;
+      document.getElementById("state").innerHTML = returnStates;
+     
+
+    }
+  }
+  async searchStatesAndCity(pincodeToSearch) {
+    let availableInfo,availableDataWithRepetition;
+    try {
+      const dataInformation = await fetch("https://young-eyrie-96553.herokuapp.com/api/data");
+      const data = await dataInformation.json();
+      availableDataWithRepetition = data.filter(info => {
+        return String(info.POSTAL_CODE).toLowerCase().includes(pincodeToSearch);
       });
-      axios
-        .get(`https://api.postalpincode.in/pincode/${e.target.value}`)
-        .then(res =>
-          this.setState({
-            state: res.data[0].PostOffice[0].State,
-            city: res.data[0].PostOffice[0].District
-          })
-        )
-        .then(() => {
-          document.getElementById("pincode").classList.remove("error");
-        })
-        .catch(err => {
-          document.getElementById("pincode").className = "error";
-          this.setState({
-            error: "Invalid PIN Code"
-          });
+
+      availableInfo = this.removeDuplicates(availableDataWithRepetition, "POSTAL_CODE");
+
+
+      if (availableInfo.length) {
+        this.outputHTML(availableInfo)
+      }
+      else {
+        this.outputHTML([]);
+      }
+      
+    }
+    catch (error) {
+      console.log(error)
+      document.getElementById("pincode").className = "error";
+        this.setState({
+          error: "Invalid PIN Code"
         });
     }
-    if (e.target.value.length !== 6) {
+   
+  }
+  
+  onChange(e) { 
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    
+    const pincodeToSearch = value.toString().toLowerCase();
+    
+
+    //covers US (4-digit)
+    if (pincodeToSearch.length >= 4 && pincodeToSearch.length <= 6) {
+      this.setState({
+        error:" "
+      })
+      this.searchStatesAndCity(pincodeToSearch);
+    }
+    else {
       this.setState({
         city: "",
         state: "",
-        error: "ZIP code must be of 6 digits"
+        error: "ZIP code must be atleast 4 digits"
       });
+      document.getElementById("city").innerHTML = `<option value="default">City</option>`;
+      document.getElementById("state").innerHTML = `<option value="default">State</option>`;
     }
   }
+  
   render() {
-    return (
-      <div style={this.props.Container}>
-        {this.state.error ? (
-          <span className="error-display">{this.state.error}</span>
-        ) : null}
-        <div style={this.props.pincodeContainer}>
-          <input
-            maxLength={6}
-            minLength={6}
-            onChange={e => this.onChange(e)}
-            name="pincode"
+  
+    const { Container, pincodeContainer, pincodeInput, cityContainer, cityInput, stateContainer, stateInput } = this.props;
+
+    const { pincode, error } = this.state;
+    
+    
+  return (
+    <div style={Container}>
+      {
+        error ? (<span className="error-display">{this.state.error}</span>): null
+      }
+
+
+      <div style={pincodeContainer}>
+        <input minLength={4} maxLength={6}  onChange={e => this.onChange(e)} name="pincode"
             placeholder="Pin Code"
-            value={this.state.pincode}
+            value={pincode}
             id="pincode"
             type="number"
-            style={this.props.pincodeInput}
+            style={pincodeInput}
           />
-        </div>
-        <div style={this.props.cityContainer}>
-          <input
-            type="String"
-            disabled={true}
-            placeholder="City"
-            value={this.state.city}
-            style={this.props.cityInput}
-          />
-        </div>
-        <div style={this.props.stateContainer}>
-          <input
-            type="String"
-            placeholder="State"
-            disabled={true}
-            value={this.state.state}
-            style={this.props.stateInput}
-          />
-        </div>
       </div>
-    );
+
+
+      <div style={cityContainer}>
+        <select name="city" type="String"  placeholder="City" 
+          style={cityInput}  id="city">
+            <option value="default">City</option>
+        </select>
+      </div>
+
+
+      <div style={stateContainer}>
+        <select name="state" type="String" placeholder="State" 
+          style={stateInput} id="state">
+          <option value="default">State</option>
+        </select>
+      </div>
+
+
+    </div>
+  );
   }
 }
 export default Pincode;
