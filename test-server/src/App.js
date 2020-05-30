ef40c7dbeac9ad2d11c998230230759155db6c51
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-05-30 20:13:56 +0530

    google maps button added (#63)

--- test-server/src/App.js
+++ test-server/src/App.js
@@ -5,7 +5,7 @@ import Pincode from "react-pincode";
 function App() {
   return (
     <div className="App">
-      <Pincode invalidError="PLease check pincode" lenghtError="check length" />
+      <Pincode invalidError="Please check pincode" lenghtError="check length" />
     </div>
   );
 }
